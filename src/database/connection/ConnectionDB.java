54c3c83fef17bd06059016d6b4fd25a7327b0df9
Valery Borys <valeryborys@gmail.com>
2020-09-10 23:16:10 +0300

    Properties changed to ResorceBundle

--- src/database/connection/ConnectionDB.java
+++ src/database/connection/ConnectionDB.java
@@ -8,20 +8,18 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
+import java.util.ResourceBundle;
 
 public class ConnectionDB {
 
 	public static Connection getConnection()
 			throws ClassNotFoundException, SQLException, FileNotFoundException, IOException {
-		String classPath = System.getProperty("user.dir");
-		String fullPropertiesPath = classPath + File.separator + "src" + File.separator + "database" + File.separator
-				+ "connection" + File.separator + "db.properties";
-		Properties prop = new Properties();
-		prop.load(new FileInputStream(fullPropertiesPath));
-		String dbUrl = prop.getProperty("db.url");
-		String dbLogin = prop.getProperty("db.login");
-		String dbPass = prop.getProperty("db.password");
-		String dbDriver = prop.getProperty("db.driverClassName");
+
+		ResourceBundle bundle = ResourceBundle.getBundle("database.connection.db");
+		String dbUrl = bundle.getString("db.url");
+		String dbLogin = bundle.getString("db.login");
+		String dbPass = bundle.getString("db.password");
+		String dbDriver = bundle.getString("db.driverClassName");
 		Class.forName(dbDriver);
 		return DriverManager.getConnection(dbUrl, dbLogin, dbPass);
 
